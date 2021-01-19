fb757291009815f42e2158da496176c01e18ce57
makrem1 <44649498+makrem1@users.noreply.github.com>
2021-01-19 10:54:45 +0100

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,50 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student_table",catalog = "student_database")
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private int roll;
+	private String name;
+	private String adress;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	//
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	
+
+}
